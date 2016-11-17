1121eca97d3932d686824b33627681078bb4ebe5
Dominic Ginger <domginger@hotmail.co.uk>
2016-11-17 21:31:14 +0000

    Week 3 Day 2 Bubble sort

--- prolog/day_2/bubble_sort.pl
+++ prolog/day_2/bubble_sort.pl
@@ -1,24 +1,17 @@
 check([]).
 check([_]).
-
 check([A,B|List]) :-
   A =< B,
   check([B|List]).
 
-bubble_sort([], []).
-%bubble_sort([_], []).
-%bubble_sort(List, _) :-
-%  check(List).
+bubble_sort(R,R) :-
+  check(R).
+bubble_sort(List,R) :-
+  swap(List,Swapped),
+  bubble_sort(Swapped,R).
 
-bubble_sort([_,_], _).
+swap([A,B|List],[B,A|List]) :- 
+  A > B.
+swap([A|List],[A|List2]) :- 
+  swap(List,List2).
 
-bubble_sort([A,B|List], R) :-
-  A =< B,
-  append(A,[List],R),
-  bubble_sort([B|List], R).
-
-bubble_sort([A,B|List], R) :-
-  A > B,
-  append(B,[List],R),
-  bubble_sort([A|List], R).
-  
