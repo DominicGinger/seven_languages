b0b43d7ae5759cb1b87925d10c1d2adffd607b27
Dominic Ginger <domginger@hotmail.co.uk>
2016-12-05 12:12:44 +0000

    Remove uneccessary variable

--- prolog/day_3/sudoku_9x9.pl
+++ prolog/day_3/sudoku_9x9.pl
@@ -1,5 +1,5 @@
 % 9x9 sudoku solver
-% sample input: 'sudoku([[_,1,_,8,6,5,_,3,_],[3,_,6,2,_,_,_,_,4],[_,7,5,_,_,_,6,_,_],[_,_,9,1,_,_,_,4,2],[5,6,_,_,_,_,_,9,3],[4,2,_,_,_,3,1,_,_],[_,_,7,_,_,_,4,1,_],[8,_,_,_,_,7,5,_,9],[_,5,_,9,8,4,_,7,_]],S).'
+% sample input: 'sudoku([[_,1,_,8,6,5,_,3,_],[3,_,6,2,_,_,_,_,4],[_,7,5,_,_,_,6,_,_],[_,_,9,1,_,_,_,4,2],[5,6,_,_,_,_,_,9,3],[4,2,_,_,_,3,1,_,_],[_,_,7,_,_,_,4,1,_],[8,_,_,_,_,7,5,_,9],[_,5,_,9,8,4,_,7,_]]).'
 
 :- use_module(library(clpfd)).
 
@@ -25,8 +25,7 @@ squares([[A,B,C|_], [D,E,F|_], [G,H,I|_]|Puzzle]) :-
   all_distinct([A,B,C,D,E,F,G,H,I]),
   squares(Puzzle).
 
-sudoku(Puzzle, Solution) :-
-  Solution = Puzzle,
+sudoku(Puzzle) :-
   length(Puzzle, Length),
   append(Puzzle, BigList), 
   BigList ins 1..Length,
@@ -35,5 +34,5 @@ sudoku(Puzzle, Solution) :-
   cols(Puzzle),
   squares(Puzzle),
 
-  pretty_print(Solution).
+  pretty_print(Puzzle).
 
